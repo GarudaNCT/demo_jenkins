ce4743087e2b5cf95c6bb521f3b50eb48f5fc44a
GarudaNCT <thanh.nc@terra-drone.co.jp>
2022-09-21 16:22:09 +0700

    make error

--- UnitTestExampleTests/UnitTestExampleTests.swift
+++ UnitTestExampleTests/UnitTestExampleTests.swift
@@ -35,7 +35,7 @@ class UnitTestExampleTests: XCTestCase {
     }
     
     func testLogin() throws {
-        let newUser = User.init(userName: "test", password: "123123")
+        let newUser = User.init(userName: "test", password: "1231234")
         let resultLogin = newUser.login()
         XCTAssertEqual(resultLogin, true, "can not login")
     }
